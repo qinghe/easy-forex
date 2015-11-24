3f044aa264ce0aeef081ff77402e4c195e9fba99
clement ledey <clement.ledey@gmail.com>
2015-11-25 00:22:02 +0100

    user model added

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151124201706) do
+ActiveRecord::Schema.define(version: 20151124232107) do
 
   create_table "exchanges", force: :cascade do |t|
     t.string   "title"
     t.integer  "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
