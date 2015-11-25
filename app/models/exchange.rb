ec4f47b405801c7c567bf58c2c0819ac9dd7311c
clement ledey <clement.ledey@gmail.com>
2015-11-25 01:00:01 +0100

    models updated

--- app/models/exchange.rb
+++ app/models/exchange.rb
@@ -1,2 +1,5 @@
 class Exchange < ActiveRecord::Base
+
+  belongs_to :user, class_name: "User", foreign_key: "user_id"
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+
+  has_many :exchanges,  foreign_key: 'user_id',  dependent: :destroy
+
 end
