cf5c254611fc06c07f4962fa1c9dfa03a594ee46
clement ledey <clement.ledey@gmail.com>
2015-12-07 12:19:40 +0100

    starting the setup

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,6 @@ class Booking < ActiveRecord::Base
   belongs_to :exchange
   belongs_to :buyer, class_name: "User", foreign_key: "buyer_id"
 
+  enumerize :status, in: [:checkout, :pending, :confirmed, :canceled], default: :checkout
+
 end
