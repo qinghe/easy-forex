243e2b1eabb81c8731390e5026a257670807a49b
baptiste mulliez <baptistem59@hotmail.com>
2015-12-07 21:15:38 +0100

    gem yahoo finance

--- Gemfile
+++ Gemfile
@@ -29,7 +29,9 @@ gem 'bootstrap-sass', '~> 3.3.1'
 gem 'font-awesome-sass', '~> 4.2.0'
 gem 'simple_form'
 gem "autoprefixer-rails"
-gem "pundit"
+
+
+gem 'yahoo-finance', '~> 1.0.0'
 
 # For user authentication
 gem 'devise'
--- app/models/exchange.rb
+++ app/models/exchange.rb
@@ -3,10 +3,21 @@ class Exchange < ActiveRecord::Base
   belongs_to :seller, class_name: "User", foreign_key: "seller_id"
 
   belongs_to :base_currency, class_name: "Currency", foreign_key: "base_currency_id"
-  belongs_to :variable_currency, class_name: "Currency", foreign_key: "base_currency_id"
+  belongs_to :variable_currency, class_name: "Currency", foreign_key: "variable_currency_id"
 
   has_one :booking, dependent: :destroy
 
   validates_presence_of :title, :amount, :seller
 
+  def get_pair_currency_rate
+    yahoo_client = YahooFinance::Client.new
+    self.rate = yahoo_client.quotes(["#{self.base_currency.code}#{self.variable_currency.code}=X"], [:ask, :bid, :last_trade_price], { raw: false })
+    calculate_exchange_amount
+  end
+
+  private
+
+  def calculate_exchange_amount
+    self.exchange_amout = self.amount * self.rate
+  end
 end
