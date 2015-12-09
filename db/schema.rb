e9b979a9ebc37cef8acc6122aaeb1b92b816d586
baptiste mulliez <baptistem59@hotmail.com>
2015-12-09 13:38:07 +0100

    basic booking creation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151207223408) do
+ActiveRecord::Schema.define(version: 20151209115240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,7 +42,6 @@ ActiveRecord::Schema.define(version: 20151207223408) do
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
     t.string   "city"
-    t.date     "meeting_date"
     t.integer  "user_id"
     t.integer  "seller_id"
     t.integer  "variable_currency_id"
@@ -50,6 +49,8 @@ ActiveRecord::Schema.define(version: 20151207223408) do
     t.float    "rate"
     t.float    "amount"
     t.float    "exchange_amount"
+    t.date     "start_date"
+    t.date     "end_date"
   end
 
   create_table "reviews", force: :cascade do |t|
